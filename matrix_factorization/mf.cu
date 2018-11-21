7faa7fbedc26d95ab77d9660a8b86b0254a5121c
nickgreenquist <nickgreenquist@gmail.com>
2018-11-20 21:46:00 -0500

    update mf.cu to be working workflow and adding python script to use trained components for recs

--- matrix_factorization/mf.cu
+++ matrix_factorization/mf.cu
@@ -1,17 +1,63 @@
 #include "matrix.h"
 #include "util.h"
+#include "training.h"
 
 int main(int argc, char **argv){
+    if(argc < 2) {
+        return -1;
+    }
+
+    // Load in data
+    string filename = argv[1];
     int rows, cols;
     float global_bias;
-    std::vector<Rating> ratings = readCSV(argv[1], &rows, &cols, &global_bias);
-    printCSV(&ratings);
-
-    // Create Sparse Matrix in Device memory
+    std::vector<Rating> ratings = readCSV(filename, &rows, &cols, &global_bias);
     cu2rec::CudaCSRMatrix* matrix = createSparseMatrix(&ratings, rows, cols);
 
-    // Kernels for factorization called here
+    // Hyperparams
+    int n_factors = 2;
+    int n_iterations = 200;
+    int seed = 42;
+    float learning_rate = 1e-3;
+    float P_reg = 1e-1;
+    float Q_reg = 1e-1;
+    float user_bias_reg = 1e-1;
+    float item_bias_reg = 1e-1;
+
+    // Create components and train on ratings
+    float *P, *Q, *losses, *user_bias, *item_bias;
+    train(matrix, n_iterations, n_factors, learning_rate, seed, &P, &Q, &losses, &user_bias, &item_bias, global_bias,
+          P_reg, Q_reg, user_bias_reg, item_bias_reg);
+
+    // Output loss every 10th iteration
+    cout << "Loss:\n";
+    for(int i = 0; i < n_iterations; ++i) {
+        if((i + 1) % 10 == 0) {
+            cout << "Iteration " << i + 1 << ": " << losses[i] << "\n";
+        }
+    }
+
+    // Write output to files
+    size_t lastindex = filename.find_last_of("."); 
+    string filepath = filename.substr(0, lastindex);
+
+    // Put global_bias into array in order to use generalized writeToFile
+    float *global_bias_array = new float[1];
+    global_bias_array[0] = global_bias;
+
+    // Write components to file
+    writeToFile(filepath, "csv", "p", P, rows, n_factors, n_factors);
+    writeToFile(filepath, "csv", "q", Q, cols, n_factors, n_factors);
+    writeToFile(filepath, "csv", "user_bias", user_bias, rows, 1, n_factors);
+    writeToFile(filepath, "csv", "item_bias", item_bias, cols, 1, n_factors);
+    writeToFile(filepath, "csv", "global_bias", global_bias_array, 1, 1, n_factors);
 
     // Free memory
     delete matrix;
+    delete [] P;
+    delete [] Q;
+    delete [] losses;
+    delete [] user_bias;
+    delete [] item_bias;
+    delete [] global_bias_array;
 }
---
+++ recommendations/get_recs.py
@@ -0,0 +1,68 @@
+import numpy as np
+import csv
+
+filename = "../data/test_ratings2"
+# Load in sparse ratings as dense matrix (hardcode dimensions for now)
+rows = 6
+cols = 5
+n_factors = 2
+ratings = [[0.0]*cols for i in range(rows)]
+with open("{}.csv".format(filename)) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    row_num = 0
+    for row in readCSV:
+        if row_num > 0:
+            u = int(row[0])
+            i = int(row[1])
+            rating = float(row[2])
+            ratings[u-1][i-1] = rating
+        row_num += 1
+ratings = np.array(ratings)
+
+# Load in all components
+P = []
+with open("{}_{}_{}.csv".format(filename, n_factors, "p")) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        user_vec = [float(row[0]), float(row[1])]
+        P.append(user_vec)
+
+Q = []
+with open("{}_{}_{}.csv".format(filename, n_factors, "q")) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        item_vec = [float(row[0]), float(row[1])]
+        Q.append(user_vec)
+
+user_bias = []
+with open("{}_{}_{}.csv".format(filename, n_factors, "user_bias")) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        b = float(row[0])
+        user_bias.append(b)
+
+item_bias = []
+with open("{}_{}_{}.csv".format(filename, n_factors, "item_bias")) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        b = float(row[0])
+        item_bias.append(b)
+
+global_bias = 0.0
+with open("{}_{}_{}.csv".format(filename, n_factors, "global_bias")) as csvfile:
+    b = csvfile.readline()
+    global_bias = float(b)
+
+# Calculate all missing ratings and output
+print("Orig Ratings:")
+print(ratings)
+print()
+for u in range(rows):
+    for i in range(cols):
+        if ratings[u][i] == 0.0:
+            pred = global_bias + user_bias[u] + item_bias[i] + np.dot(np.transpose(P[u]), Q[i])
+            ratings[u][i] = pred
+
+np.set_printoptions(precision=2)
+print("Predicted Ratings:")
+print(ratings)
