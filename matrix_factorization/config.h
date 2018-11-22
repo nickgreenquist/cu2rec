17f9befde03f2fd0911cacdd1f0ac87e84b66803
dorukkilitcioglu <doruketc@gmail.com>
2018-11-22 09:45:08 -0500

    Added support for passing config to constant memory

--- matrix_factorization/config.h
+++ matrix_factorization/config.h
@@ -5,22 +5,35 @@
 
 using namespace std;
 
-class Config {
-public:
-    int cur_iterations = 0;
-    int total_iterations = 1000;
-    int n_factors = 10;
-    float learning_rate = 1e-4;
-    int seed = 42;
-    float P_reg = 1e-1;
-    float Q_reg = 1e-1;
-    float user_bias_reg = 1e-1;
-    float item_bias_reg = 1e-1;
+namespace config {
+    // CUDA variables
+    __constant__ int cur_iterations = 0;
+    __constant__ int total_iterations = 1000;
+    __constant__ int n_factors = 10;
+    __constant__ float learning_rate = 1e-4;
+    __constant__ int seed = 42;
+    __constant__ float P_reg = 1e-1;
+    __constant__ float Q_reg = 1e-1;
+    __constant__ float user_bias_reg = 1e-1;
+    __constant__ float item_bias_reg = 1e-1;
 
-    bool read_config(string file_path);
-    bool write_config(string file_path);
-    bool set_cuda_variables();
-    bool get_cuda_variables();
-};
+    class Config {
+        public:
+            int cur_iterations = 0;
+            int total_iterations = 1000;
+            int n_factors = 10;
+            float learning_rate = 1e-4;
+            int seed = 42;
+            float P_reg = 1e-1;
+            float Q_reg = 1e-1;
+            float user_bias_reg = 1e-1;
+            float item_bias_reg = 1e-1;
+
+            bool read_config(string file_path);
+            bool write_config(string file_path);
+            bool set_cuda_variables();
+            bool get_cuda_variables();
+    };    
+}
 
 #endif
--- matrix_factorization/tests/test_config.cu
+++ matrix_factorization/tests/test_config.cu
@@ -6,7 +6,7 @@ string file_path = "../../data/test/test_config.cfg";
 string gen_path = "../../data/test/gen/test_config.cfg";
 
 void test_config_load(string file_path) {
-    Config *cfg = new Config();
+    config::Config *cfg = new config::Config();
     cfg->read_config(file_path);
     assert(cfg->total_iterations == 100);
     assert(fabs(cfg->P_reg - 2e-1) < 1e-4);
@@ -14,11 +14,27 @@ void test_config_load(string file_path) {
 }
 
 void test_config_save() {
-    Config *cfg = new Config();
+    config::Config *cfg = new config::Config();
     cfg->total_iterations = 100;
     cfg->P_reg = 2e-1;
     cfg->write_config(gen_path);
     test_config_load(gen_path);
+    delete cfg;
+}
+
+void test_config_set_cuda_variables() {
+    config::Config *cfg = new config::Config();
+    cfg->total_iterations = 100;
+    cfg->P_reg = 2e-1;
+    assert(cfg->set_cuda_variables());
+    delete cfg;
+}
+
+void test_config_get_cuda_variables() {
+    config::Config *cfg = new config::Config();
+    cfg->total_iterations = 100;
+    cfg->P_reg = 2e-1;
+    assert(cfg->get_cuda_variables());
     delete cfg;
 }
 
@@ -28,6 +44,12 @@ int main() {
     cout << "PASSED\n";
     cout << "Testing config save...\n";
     test_config_save();
+    cout << "PASSED\n";
+    cout << "Testing config cuda set variables...\n";
+    test_config_set_cuda_variables();
+    cout << "PASSED\n";
+    cout << "Testing config cuda get variables...\n";
+    test_config_get_cuda_variables();
     cout << "PASSED\n";
 
     return 0;
