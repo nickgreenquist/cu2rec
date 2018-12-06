153b3fab4bafb4d460a258fef3c676d52a896c42
nickgreenquist <nickgreenquist@gmail.com>
2018-12-06 12:50:29 -0500

    add n_threads to config, clean up minor other things

--- matrix_factorization/loss.h
+++ matrix_factorization/loss.h
@@ -18,5 +18,4 @@ __global__ void total_loss_kernel(float *in_errors, float *out_errors, int n_err
 // function headers
 std::tuple<float, float> get_error_metrics_cpu(float *errors, float *errors_device, int n_errors);
 std::tuple<float, float> get_error_metrics_gpu(float *in_errors, float *out_errors, float *out_errors_host, int n_errors, int grid_size, int block_size);
-void calculate_loss_gpu(CudaDenseMatrix* P_d, CudaDenseMatrix* Q_d, int factors, int user_count, int item_count, int num_ratings, CudaCSRMatrix* matrix, float * error_d, float * user_bias, float * item_bias, float global_bias);
-float calculate_total_loss_gpu(float *in_errors, float *out_errors, float *out_errors_host, int n_errors, int grid_size, int block_size, ErrorType error_type);
+void calculate_loss_gpu(CudaDenseMatrix* P_d, CudaDenseMatrix* Q_d, config::Config* cfg, int user_count, int item_count, int num_ratings, CudaCSRMatrix* matrix, float * error_d, float * user_bias, float * item_bias, float global_bias);
