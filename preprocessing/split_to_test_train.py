2aaad27232b2a2a8afe0cb5299c2913db38718f2
dorukkilitcioglu <doruketc@gmail.com>
2018-12-08 18:29:22 -0500

    Added comments to split code

--- preprocessing/split_to_test_train.py
+++ preprocessing/split_to_test_train.py
@@ -1,7 +1,12 @@
+"""Splits a csv file into training and test sets
+"""
+
 import argparse
 import csv
 import os
 import random
+
+from map_items import write_to_file
 
 
 def split_per_user(rows, train_percent):
@@ -44,16 +49,6 @@ def split_true(rows, train_percent):
     return train, test
 
 
-def write_ratings(filename, ratings):
-    with open(filename, "w", newline='') as file:
-        file.write("userId,itemId,rating\n")
-        for row in ratings:
-            row = [str(i) for i in row]
-            line = ",".join(row)
-            file.write(line)
-            file.write('\n')
-
-
 def read_ratings(filename):
     rows = []
     # userId, itemId, rating, <other ignored columns>
@@ -75,12 +70,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Splits a csv file into training and test sets")
     parser.add_argument('file_ratings', type=str, help='the path to the ratings file to split')
     parser.add_argument('test_ratio', type=float, help='the ratio of test examples')
+    parser.add_argument('-s', '--seed', type=int, default=42, help='the random seed')
     args = parser.parse_args()
 
+    random.seed(args.seed)
     rows = read_ratings(args.file_ratings)
     train, test = split_true(rows, 1 - args.test_ratio)
 
     filepath, extension = os.path.splitext(args.file_ratings)
 
-    write_ratings("{}_train{}".format(filepath, extension), train)
-    write_ratings("{}_test{}".format(filepath, extension), test)
+    write_to_file("{}_train{}".format(filepath, extension), train)
+    write_to_file("{}_test{}".format(filepath, extension), test)
