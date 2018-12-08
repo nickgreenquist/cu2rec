aecb6c79f8d6b209cb418f0b8912ecdda8a536c7
dorukkilitcioglu <doruketc@gmail.com>
2018-12-08 18:11:56 -0500

    Reuse code in mapping scripts

--- preprocessing/map_items.py
+++ preprocessing/map_items.py
@@ -1,47 +1,100 @@
+"""Maps items in a ratings CSV file to sequential user and item ids.
+
+By default, assumes the first column is user id, second is item id,
+and third is rating. If different, a different function can be passed into
+map_rows as the get_info parameter.
+"""
+
 import csv
 
-filename = "../data/ratings.csv"
 
-# maps itemId from real id to id 0-6000
-itemId_map = {}
+def get_sequential_info(row):
+    # userId, itemId, rating, <other ignored columns>
+    user_id = int(row[0])
+    item_id = int(row[1])
+    rating = float(row[2])
+    return user_id, item_id, rating
 
-unique_itemIds = set()
-unique_userIds = set()
 
-# userId, itemId, rating, <other ignored columns>
-rows = []
-with open(filename) as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    row_num = 0
-    for row in readCSV:
-        if row_num > 0:
-            userId = int(row[0])
-            itemId = int(row[1])
-            rating = float(row[2])
+def map_rows(filename, user_mapping, item_mapping, delimiter=',', has_header=True,
+             get_info=get_sequential_info, add_missing=True):
+    """Maps the rows of the file with user_mapping and item_mapping.
 
-            rows.append([userId, itemId, rating])
+    If add_missing is True, it will automatically add the missing user and
+    item ids to the mappings. They are mapped sequentially starting from 1.
+    get_info is a function callback that returns the userid-itemid-rating tuple
+    from a row.
+    """
+    rows = []
+    missing_users = 0
+    missing_items = 0
+    with open(filename) as csvfile:
+        csv_rows = csv.reader(csvfile, delimiter=delimiter)
+        if has_header:
+            next(csv_rows, None)
+        for row in csv_rows:
+            user_id, item_id, rating = get_info(row)
 
-            unique_itemIds.add(itemId)
-            unique_userIds.add(userId)
-        row_num += 1
+            if user_id not in user_mapping:
+                if add_missing:
+                    user_mapping[user_id] = len(user_mapping) + 1
+                else:
+                    missing_users += 1
+                    continue
+            user_id = user_mapping[user_id]
 
-# For each unique item id, map to next available id starting from 1
-itemIds = sorted(list(unique_itemIds))
-next_avail_id = 1
-for id in  itemIds:
-    itemId_map[id] = next_avail_id
-    next_avail_id += 1
+            if item_id not in item_mapping:
+                if add_missing:
+                    item_mapping[item_id] = len(item_mapping) + 1
+                else:
+                    missing_items += 1
+                    continue
+            item_id = item_mapping[item_id]
 
-# Update itemId in each row to be between 0-9000
-for row in rows:
-    row[1] = itemId_map[row[1]]
+            rows.append([user_id, item_id, rating])
+    if missing_users > 0:
+        print("Skipped %d rows because of missing users" % missing_users)
+    if missing_items > 0:
+        print("Skipped %d rows because of missing items" % missing_items)
+    return rows
 
-filename = '../data/ratings_mapped.csv'
-with open(filename, "w", newline='') as file:
-    file.write("userId,itemId,rating\n")
+
+def sort_by_user(rows):
+    """Sort the rows by user
+    """
+    user_map = {}
     for row in rows:
-        row = [str(i) for i in row]
-        line = ",".join(row)
-        file.write(line)
-        file.write('\n')
+        if row[0] in user_map:
+            user_map[row[0]].append(row)
+        else:
+            user_map[row[0]] = [row]
+    sorted_rows = []
+    for user_id in sorted(list(user_map)):
+        for row in user_map[user_id]:
+            sorted_rows.append(row)
+    return sorted_rows
 
+
+def write_to_file(filename, rows):
+    """Write the user item rating tuple to file
+    """
+    with open(filename, "w", newline='') as file:
+        file.write("userId,itemId,rating\n")
+        for row in rows:
+            row = [str(i) for i in row]
+            line = ",".join(row)
+            file.write(line)
+            file.write('\n')
+
+
+def process_file(filename_in, filename_out):
+    user_mapping = {}
+    item_mapping = {}
+    rows = sort_by_user(map_rows(filename_in, user_mapping, item_mapping))
+    write_to_file(filename_out, rows)
+
+
+if __name__ == '__main__':
+    filename_in = "../data/ratings.csv"
+    filename_out = "../data/ratings_mapped.csv"
+    process_file(filename_in, filename_out)
